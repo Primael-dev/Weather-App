fe07a57e8c91c4f3d9e86f3ac70e5aff5d593e88
Primael BANKOLE <bankolewilli@gmail.com>
2026-06-30 23:44:06 +0100

    review de app.py

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
 # Importation des modules
-from flask import Flask
+import os
+from flask import Flask, send_from_directory
 from backend.routes import api_bp
 from flask_cors import CORS
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="frontend", static_url_path="")
 CORS(app)
 
 # Enregistrement de la blueprint
 app.register_blueprint(api_bp)
 
+@app.route("/")
+def index():
+    return send_from_directory(app.static_folder, "index.html")
+
 if __name__ == "__main__":
-
-
     app.run(debug=True, port=5000)
